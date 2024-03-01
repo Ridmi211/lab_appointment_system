b6515313f950610209648ab2a7bdcb5acd0f20db
Ridmi211 <ridmiyatigammana@gmail.com>
2024-03-01 23:45:01 +0530

    Add test type

--- src/main/java/com/labSchedulerSystem/controller/UserController.java
+++ src/main/java/com/labSchedulerSystem/controller/UserController.java
@@ -255,7 +255,17 @@ public class UserController extends HttpServlet {
 		user.setEducationalQualifications(request.getParameter("educationalQualifications"));
 		user.setSpecializedJobs(request.getParameter("specializedJobs"));
 		user.setAccessRight(AccessRight.valueOf(request.getParameter("accessRight")));
-		user.setSelectedTestType(Test.TestType.valueOf(request.getParameter("selectedTestType")));
+		/*
+		 * user.setSelectedTestType(Test.TestType.valueOf(request.getParameter(
+		 * "selectedTestType")));
+		 */
+		
+		  if (user.getAccessRight() == AccessRight.ROLE_TECHNITIAN) {
+		        user.setSelectedTestType(Test.TestType.valueOf(request.getParameter("selectedTestType")));
+		    } else {
+		        // Set the selectedTestType to default if the user's role is not ROLE_TECHNITIAN
+		        user.setSelectedTestType(Test.TestType.DEFAULT);
+		    }
 		try {
 			if (getUserService().editUser(user)) {
 				UserService.sendUserUpdateEmail(user);
